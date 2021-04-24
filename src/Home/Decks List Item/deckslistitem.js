7189f0809819851ad7eb56a00b84566a0d7064d6
xJIsaac <xjisaac@gmail.com>
2021-04-24 16:27:13 -0500

    Completed deckslistitem component and migrated code from Home.js to it

---
+++ src/Home/Decks List Item/deckslistitem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function DeckListItem({ id, name, cards, description }, index) {
+  return (
+    <div className="list-group-item" key={index}>
+      <div className="d-flex justify-content-between">
+        <h5>{name}</h5>
+        <small>{cards.length} Cards</small>
+      </div>
+      <p>{description}</p>
+
+      <div className="d-flex justify-content-between">
+        <div>
+          <Link to={`/decks/${id}/edit`}>
+            <button type="button" className="btn btn-secondary mr-3">
+              <i className="bi bi-pencil"></i> Edit
+            </button>
+          </Link>
+          <Link to={`/decks/${id}/study`}>
+            <button type="button" className="btn btn-primary">
+              <i className="bi bi-book"></i> Study
+            </button>
+          </Link>
+        </div>
+        <Link to="#">
+          <button type="button" className="btn btn-danger">
+            <i className="bi bi-trash"></i>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/Home/home.js
+++ src/Home/home.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { listDecks } from "../utils/api/index.js";
+import DeckListItem from "./Decks List Item/deckslistitem.js";
 /*
 A "Create Deck" button is shown and clicking it brings the user to the Create Deck screen.
 Existing decks are each shown with the deck name, the number of cards, and a “Study,” “View,” and “Delete” button.
@@ -7,52 +9,37 @@ Clicking the “Study” button brings the user to the Study screen.
 Clicking the “Edit” button brings the user to the Edit Deck screen.
 Clicking the “Delete” button shows a warning message before deleting the deck.
 */
-const deckId = 1;
 
 function Home() {
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    const abortCtrl = new AbortController();
+    listDecks(abortCtrl.signal).then((data) => {
+      setDecks(data);
+    });
+
+    return () => {
+      abortCtrl.abort();
+    };
+  }, []);
+
   return (
     <div>
       {/* Create Button */}
-      <div class="mb-3">
+      <div className="mb-3">
         <Link to="/decks/new">
-          <button type="button" class="btn btn-secondary">
-            <i class="bi bi-plus-circle"></i> Create Deck
+          <button type="button" className="btn btn-secondary">
+            <i className="bi bi-plus-circle"></i> Create Deck
           </button>
         </Link>
       </div>
 
-      {/* Card List */}
-      <div class="card mb-3">
-        <div class="card-body">
-          <div class="mb-3">
-            <h5 class="card-title">Card title</h5>
-
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-
-          <div class="d-flex justify-content-between">
-            <div>
-              <Link to={`/decks/${deckId}/edit`}>
-                <button type="button" class="btn btn-secondary mr-3">
-                  <i class="bi bi-pencil"></i> Edit
-                </button>
-              </Link>
-              <Link to={`/decks/${deckId}/study`}>
-                <button type="button" class="btn btn-primary">
-                  <i class="bi bi-book"></i> Study
-                </button>
-              </Link>
-            </div>
-            <Link to="#">
-              <button type="button" class="btn btn-danger">
-                <i class="bi bi-trash"></i>
-              </button>
-            </Link>
-          </div>
-        </div>
+      {/* List of Decks */}
+      <div className="list-group mb-5">
+        {decks.map((deck, index) => {
+          return DeckListItem(deck, index);
+        })}
       </div>
     </div>
   );
