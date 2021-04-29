d6cdb9283f42ab72d0c946ad4846d52f98881d7b
xJIsaac <xjisaac@gmail.com>
2021-04-29 00:49:14 -0500

    Update createDeck.js

--- src/Create Deck/createDeck.js
+++ src/Create Deck/createDeck.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router";
+import React, { useState } from "react";
+import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
 import { createDeck } from "../utils/api/index.js";
 
