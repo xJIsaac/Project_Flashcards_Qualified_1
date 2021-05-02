55130c58c03af8b05babf8fc3a6fa8dadd7c58dc
xJIsaac <xjisaac@gmail.com>
2021-05-02 00:24:45 -0500

    Changed breadcrumb file name

--- src/Edit Deck/EditDeckBreadcrumb.js
+++ src/Edit Deck/EditDeckBreadcrumb.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function EditBreadcrumb({ deck }) {
+export default function EditDeckBreadcrumb({ deck }) {
   return (
     <div>
       <nav aria-label="breadcrumb">
